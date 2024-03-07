a097beb48387a80e1248b28b094c5e42fafcd279
Youssef Fekry <yousseffekry14@gmail.com>
2024-03-07 20:46:58 +0200

    lib updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,19 @@
+import 'package:chat_app_with_api/screens/login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ChatApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ChatApp extends StatelessWidget {
+  const ChatApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Container(),
+        body: LoginPage(),
       ),
     );
   }
